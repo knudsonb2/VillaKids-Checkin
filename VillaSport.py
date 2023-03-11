e9a7b757b90583e78208fd8e458e1aa8d1424d9b
Nicklas Knudson <nicklasbknudson@gmail.com>
2023-03-11 14:11:00 -0800

    loaded selenium webdriver

---
+++ VillaSport.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+def main():
+    driver = webdriver.Chrome()
+    driver.get('https://villasport.motionvibe.com/(S(03l5zjksx4emb2pzxdfe4t5b))/MemberPortal.aspx')
+
+if __name__ == '__main__':
+    main()
